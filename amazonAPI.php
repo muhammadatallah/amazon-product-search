10ce84b0800fd161007abba93fe0a9335fb2b27d
Muhammad.Atallah <muhammad.atallah.eg@gmail.com>
2017-08-27 12:49:36 +0200

    Adding associatedId and .gitignore file

--- amazonAPI.php
+++ amazonAPI.php
@@ -7,7 +7,7 @@ require('vendor/autoload.php');
 include 'secretKeys.inc';
 session_start();
 
-$urlBuilder = new AmazonUrlBuilder($keyId, $secretKey, $_GET['country']);
+$urlBuilder = new AmazonUrlBuilder($keyId, $secretKey,$associateId,$_GET['country']);
 // Setup a new instance of the AmazonAPI and define the type of response
 $amazonAPI = new AmazonAPI($urlBuilder, 'simple');
 $items = $amazonAPI->ItemSearch($_GET['search'], $_GET['category']);
